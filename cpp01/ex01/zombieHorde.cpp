19d1295d2dcbfcf67c91b2f9f1f557c2a6b53ba2
adeline <adelineeminian13@gmail.com>
2024-10-06 20:34:44 +0400

    cpp01 done

--- cpp01/ex01/zombieHorde.cpp
+++ cpp01/ex01/zombieHorde.cpp
@@ -4,7 +4,7 @@ Zombie* zombieHorde(int N, std::string name)
 {
 	Zombie *z;
 	z = new Zombie[N];
-	for (int i = 0; i < N; i++){
+	for (int i = 0; i < N; i++) {
 		z[i].setName(name + std::to_string(i + 1));
 		z[i].announce();
 	}

e430843f50834cc2254816dc400990ad5420a5af
Adeline <adelineeminian13@gmail.com>
2024-12-13 15:39:28 +0400

    ex02 main  done

--- cpp05/ex02/main.cpp
+++ cpp05/ex02/main.cpp
@@ -1,34 +1,180 @@
 #include <iostream>
 #include "Bureaucrat.hpp"
 #include "ShrubberyCreationForm.hpp"
+#include "PresidentialPardonForm.hpp"
 #include "RobotomyRequestForm.hpp"
 
+using std::cout;
+using std::endl;
+
+
 int main() {
-	// try {
-		// ShrubberyCreationForm shrubForm("home");
-		// Bureaucrat highRanker("Alice", 1);
-		// shrubForm.beSigned(highRanker);
-		// highRanker.signForm(shrubForm);
-		// shrubForm.execute(highRanker);
-
-	// } catch (std::exception &e) {
-	// 	// Catch and print any exceptions
-	// 	std::cerr << "Error: " << e.what() << '\n';
-	// }
-
-	try
+	cout << endl << "\033[1;34m################# TESTING ###################\033[0m" << endl << endl;
 	{
-		RobotomyRequestForm rob("robik");
-		Bureaucrat highRanker("bob", 10);
-		rob.beSigned(highRanker);
-		highRanker.signForm(rob);
-		rob.execute(highRanker);
-		highRanker.executeForm(rob);
+		std::string	form = "AWS";
+		std::string	Name = "bglik";
+		std::string target = "burger";
+		int			Grade = 1; //High enough for sign end execute
+
+		try {
+			cout << "\033[32mTrying Bureaucrat " << Name << " with " << Grade << " grade to sign Shrubbery Creation -------> \033[0m" ;
+			Bureaucrat				highRanker(Name, Grade);
+			ShrubberyCreationForm	shrubForm(target);
+
+			cout << endl << "\033[35mRequired grade for sign the form is 145 and for execute is 137. \033[0m" << endl;
+			shrubForm.beSigned(highRanker);
+			try {
+				cout << endl;
+				highRanker.signForm(shrubForm);
+				cout << endl;
+
+			}
+			catch(const Bureaucrat::GradeTooLowException& e) {
+				std::cerr << e.what() << '\n';
+			}
+			try {
+				shrubForm.execute(highRanker);
+			}
+			catch(const ShrubberyCreationForm::FormNotSignedException& e) {
+				std::cerr << e.what() << '\n';
+			}
+			try {
+				cout << endl;
+				highRanker.executeForm(shrubForm);
+				cout << endl;
+			}
+			catch(const Bureaucrat::GradeTooLowException& e) {
+				std::cerr << e.what() << '\n';
+			}
+		}
+		catch (std::exception &e) {
+			std::cerr << "Error: " << e.what() << '\n';
+		}
 	}
-	catch(const std::exception& e)
+	cout << endl << "\033[33m>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\033[0m" << endl << endl;
 	{
-		std::cerr << e.what() << '\n';
+		std::string	form = "AWS";
+		std::string	Name = "Duck";
+		std::string target = "fish";
+		int			Grade = 149; 
+
+		try {
+			cout << "\033[32mTrying Bureaucrat " << Name << " with " << Grade << " grade to sign Shrubbery Creation -------> \033[0m" ;
+			Bureaucrat				highRanker(Name, Grade);
+			ShrubberyCreationForm	shrubForm(target);
+
+			cout << endl << "\033[35mRequired grade for sign the form is 145 and for execute is 137. \033[0m" << endl;
+			shrubForm.beSigned(highRanker);
+			try {
+				cout << endl;
+				highRanker.signForm(shrubForm);
+				cout << endl;
+
+			}
+			catch(const Bureaucrat::GradeTooLowException& e) {
+				std::cerr << e.what() << '\n';
+			}
+			try {
+				shrubForm.execute(highRanker);
+			}
+			catch(const ShrubberyCreationForm::FormNotSignedException& e) {
+				std::cerr << e.what() << '\n';
+			}
+			try {
+				cout << endl;
+				highRanker.executeForm(shrubForm);
+				cout << endl;
+			}
+			catch(const Bureaucrat::GradeTooLowException& e) {
+				std::cerr << e.what() << '\n';
+			}
+		}
+		catch (std::exception &e) {
+			std::cerr << "Error: " << e.what() << '\n';
+		}
 	}
-	
-	return 0;
+	cout << endl << "\033[33m>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\033[0m" << endl << endl;
+	{
+		std::string	form = "AWS";
+		std::string	Name = "Bglik";
+		std::string target = "qyabab";
+		int			Grade = 30;
+
+		try {
+			cout << "\033[32mTrying Bureaucrat " << Name << " with " << Grade << " grade to sign Presidential Pardon -------> \033[0m" ;
+			Bureaucrat				highRanker(Name, Grade);
+			PresidentialPardonForm	shrubForm(target);
+
+			cout << endl << "\033[35mRequired grade for sign the form is 25 and for execute is 5. \033[0m" << endl;
+			shrubForm.beSigned(highRanker);
+			try {
+				cout << endl;
+				highRanker.signForm(shrubForm);
+				cout << endl;
+
+			}
+			catch(const Bureaucrat::GradeTooLowException& e) {
+				std::cerr << e.what() << '\n';
+			}
+			try {
+				shrubForm.execute(highRanker);
+			}
+			catch(const PresidentialPardonForm::FormNotSignedException& e) {
+				std::cerr << e.what() << '\n';
+			}
+			try {
+				cout << endl;
+				highRanker.executeForm(shrubForm);
+				cout << endl;
+			}
+			catch(const Bureaucrat::GradeTooLowException& e) {
+				std::cerr << e.what() << '\n';
+			}
+		}
+		catch (std::exception &e) {
+			std::cerr << "Error: " << e.what() << '\n';
+		}
+	}
+	cout << endl << "\033[33m>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\033[0m" << endl << endl;
+	{
+		std::string	form = "AWS";
+		std::string	Name = "Bglik";
+		std::string target = "pizza";
+		int			Grade = 19;
+
+		try {
+			cout << "\033[32mTrying Bureaucrat " << Name << " with " << Grade << " grade to sign Robotomy REquest -------> \033[0m" ;
+			Bureaucrat				highRanker(Name, Grade);
+			RobotomyRequestForm	shrubForm(target);
+
+			cout << endl << "\033[35mRequired grade for sign the form is 72 and for execute is 45. \033[0m" << endl;
+			shrubForm.beSigned(highRanker);
+			try {
+				cout << endl;
+				highRanker.signForm(shrubForm);
+				cout << endl;
+
+			}
+			catch(const Bureaucrat::GradeTooLowException& e) {
+				std::cerr << e.what() << '\n';
+			}
+			try {
+				shrubForm.execute(highRanker);
+			}
+			catch(const RobotomyRequestForm::FormNotSignedException& e) {
+				std::cerr << e.what() << '\n';
+			}
+			try {
+				cout << endl;
+				highRanker.executeForm(shrubForm);
+				cout << endl;
+			}
+			catch(const Bureaucrat::GradeTooLowException& e) {
+				std::cerr << e.what() << '\n';
+			}
+		}
+		catch (std::exception &e) {
+			std::cerr << "Error: " << e.what() << '\n';
+		}
+	}
 }

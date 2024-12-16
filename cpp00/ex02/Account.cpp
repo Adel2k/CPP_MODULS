da078e2cecdc4c4f12b7af86bc46f6d78a4626a8
Adeline <adelineeminian13@gmail.com>
2024-12-16 16:51:06 +0400

    edited

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -8,11 +8,9 @@ int Account::_totalAmount = 0;
 int Account::_totalNbDeposits = 0;
 int Account::_totalNbWithdrawals = 0;
 
-Account::Account(int initial_deposit)
+Account::Account(int initial_deposit) : _accountIndex(_nbAccounts++), _amount(initial_deposit), _nbDeposits(0), _nbWithdrawals(0) 
 {
 	_displayTimestamp();
-	_accountIndex = _nbAccounts++;
-	_amount = initial_deposit;
 	_totalAmount += initial_deposit;
 	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";created" << std::endl;
 }

d242e565a07006e9e5cc1b355dca8e893b9feeac
Adeline <adelineeminian13@gmail.com>
2025-01-10 15:46:30 +0400

    done

---
+++ cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,40 @@
+#ifndef MutantStack_HPP
+# define MutantStack_HPP
+
+# include <iostream>
+# include <stack>
+# include <deque>
+
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+    private:
+
+    public:
+        MutantStack();
+        MutantStack(const MutantStack& other);
+        ~MutantStack();
+        typedef std::deque<T> container;
+        typedef typename container::iterator iterator;
+        typedef typename container::const_iterator const_iterator;
+        typedef typename container::reverse_iterator reverse_iterator;
+        typedef typename container::const_reverse_iterator const_reverse_iterator;
+
+        iterator    begin();
+        iterator    end();
+        const_iterator begin() const;
+        const_iterator end() const;
+        reverse_iterator rbegin();
+        reverse_iterator rend();
+        const_reverse_iterator rbegin() const;
+        const_reverse_iterator rend() const;
+
+
+
+        MutantStack& operator=(const MutantStack& other);
+};
+
+# include "MutantStack.tpp"
+
+#endif
--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -1,32 +1,61 @@
-int main()
-{
-    MutantStack<int> mstack;
+#include "MutantStack.hpp"
 
-    mstack.push(5);
-    mstack.push(17);
-    
-    std::cout << mstack.top() << std::endl;
-    
-    mstack.pop();
-    
-    std::cout << mstack.size() << std::endl;
-    
-    mstack.push(3);
-    mstack.push(5);
-    mstack.push(737);
-    //[...]
-    mstack.push(0);
-    
-    MutantStack<int>::iterator it = mstack.begin();
-    MutantStack<int>::iterator ite = mstack.end();
-    
-    ++it;
-    --it;
-    while (it != ite)
-    {
-        std::cout << *it << std::endl;
-        ++it;
-    }
-    std::stack<int> s(mstack);
-    return 0;
+int	main(void) {
+	MutantStack<int> mstack;
+
+	mstack.push(5);
+	mstack.push(17);
+
+	std::cout << "\033[31m***********************************************\033[0m" << std::endl;
+	std::cout << std::endl;
+	std::cout << mstack.top() << std::endl;
+
+	mstack.pop();
+
+	std::cout << mstack.size() << std::endl;
+	std::cout << "\033[31m***********************************************\033[0m" << std::endl;
+	std::cout << std::endl;
+
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	mstack.push(0);
+
+
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	std::cout << "\033[33m***********************************************\033[0m" << std::endl;
+	std::cout << std::endl;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::cout << "\033[33m***********************************************\033[0m" << std::endl;
+	std::cout << std::endl;
+
+	std::stack<int>	tmp(mstack);
+	MutantStack<int>::iterator tit = mstack.begin();
+	MutantStack<int>::iterator tite = mstack.end();
+	while (tit != tite)
+	{
+		std::cout << *tit << std::endl;
+		++tit;
+	}
+	std::cout << "\033[35m***********************************************\033[0m" << std::endl;
+	std::cout << std::endl;
+	MutantStack<int> s(mstack);
+	MutantStack<int>::const_reverse_iterator sit = s.rbegin();
+	MutantStack<int>::const_reverse_iterator site = s.rend();
+	std::cout << "\033[33m***********************************************\033[0m" << std::endl;
+	std::cout << std::endl;
+	while (sit != site)
+	{
+		std::cout << *sit << std::endl;
+		++sit;
+	}
+	std::cout << "\033[33m***********************************************\033[0m" << std::endl;
+	std::cout << std::endl;
+
+	return (0);
 }
